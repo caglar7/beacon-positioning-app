a75b1e8e3649f177faba974c5c7814f48eadc4e4
caglar7 <mcaglarcnkr@gmail.com>
2021-01-09 20:29:05 +0300

    added canvas as view object in a LinearLayout

--- app/src/main/java/com/example/movement_drawing/MainActivity.java
+++ app/src/main/java/com/example/movement_drawing/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.movement_drawing;
 
 import android.os.Bundle;
-
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // for log statements
+    private static final String TAG = "MainActivity";
+
+    LinearLayout canvasLinearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PaintView paintView = new PaintView(this);
-        setContentView(paintView);
+        setContentView(R.layout.activity_main);
+
+        canvasLinearLayout = findViewById(R.id.canvasViewLayout);
+        PaintView paintView = new PaintView(getApplicationContext());
+        canvasLinearLayout.addView(paintView);
     }
 }
--- app/src/main/java/com/example/movement_drawing/PaintView.java
+++ app/src/main/java/com/example/movement_drawing/PaintView.java
@@ -1,17 +1,27 @@
 package com.example.movement_drawing;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+// for test cases
+// with timer user will be moving at 1m/s
+// direction will be the inputs
+
+// initially screen width is 10 meters, calculate that way
+
+// attention on y direction
+
 public class PaintView extends View {
+    private static final String TAG = "PaintView";
 
     public LayoutParams params;
     private Path path = new Path();
@@ -19,10 +29,22 @@ public class PaintView extends View {
     private Paint brushIcon = new Paint();
     private float pointX;
     private float pointY;
+    private float incrementPerMeter;
+    private float screenScale = 10f;       // 10 meters for canvas width
+
+    private Timer updateTimer;
+    private long updatePeriod = 1000l;
+
+    // at first, horizontal right is direction
+    private int horizontalDir = 1;
+    private int verticalDir = 0;
+
+    private boolean checkStart = false;
 
     // constructor of the class here
     public PaintView(Context context) {
         super(context);
+        checkStart = true;
 
         brush.setAntiAlias(true);
         brush.setColor(Color.GRAY);
@@ -36,11 +58,59 @@ public class PaintView extends View {
         brushIcon.setStrokeJoin(Paint.Join.ROUND);
         brushIcon.setStrokeWidth(50f);
 
-        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+        // run method every second
+        updateTimer = new Timer();
+        updateTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(checkStart == false)
+                {
+                    updateTheDrawing();
+                }
+            }
+        }, 0, updatePeriod);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        // put pointer on the center at first
+        // init increment per meter in terms of screen coordinates
+        if(checkStart)
+        {
+            checkStart = false;
+            pointX = canvas.getWidth()/2;
+            pointY = canvas.getHeight()/2;
+            incrementPerMeter = ( (float) canvas.getWidth()) / screenScale;
+            path.moveTo(pointX, pointY);
+        }
+
+        canvas.drawPath(path, brush);
+        canvas.drawCircle(pointX, pointY, 10, brushIcon);
+    }
+
+    private void updateTheDrawing()
+    {
+        // set direction
+        // initially there is no user input
+        horizontalDir = 1;
+        verticalDir = 0;
+
+        // update points, paths
+        pointX += (horizontalDir) * incrementPerMeter;
+        pointY += (-verticalDir) * incrementPerMeter;
+        path.lineTo(pointX, pointY);
+
+        // refresh canvas
+        postInvalidate();
+    }
+
 
     // TOUCH FOR TESTING IF IT WORKS
     // DELETE LATER
+    /*
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         // get the point of touch
@@ -63,10 +133,5 @@ public class PaintView extends View {
         postInvalidate();
         return false;
     }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawPath(path, brush);
-        canvas.drawCircle(pointX, pointY, 10, brushIcon);
-    }
+     */
 }

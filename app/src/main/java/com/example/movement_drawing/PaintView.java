13015d0405c57b8f3df88d6add732762eeaf4c9b
caglar7 <mcaglarcnkr@gmail.com>
2021-01-10 23:36:06 +0300

    tried methods to add direction arrow, then chose put an image

--- app/src/main/java/com/example/movement_drawing/PaintView.java
+++ app/src/main/java/com/example/movement_drawing/PaintView.java
@@ -5,10 +5,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,6 +36,9 @@ public class PaintView extends View {
     private Path path = new Path();
     private Paint brush = new Paint();
     private Paint brushIcon = new Paint();
+    private Paint brushArrow = new Paint();
+    private float iconRadius = 10f;
+
     private float pointX;
     private float pointY;
     private float incrementPerMeter;
@@ -39,6 +50,10 @@ public class PaintView extends View {
     // at first, horizontal right is direction
     private int horizontalDir = 1;
     private int verticalDir = 0;
+
+    // FOR DIRECTION ILLUSTRATION
+    private Drawable directionImage;
+    private float imageRange;
 
     // Colors
     private Color colorBackground;
@@ -55,6 +70,7 @@ public class PaintView extends View {
         brush.setColor(getResources().getColor(R.color.colorPath));
         brush.setStyle(Paint.Style.STROKE);
         brush.setStrokeJoin(Paint.Join.ROUND);
+        brush.setStrokeCap(Paint.Cap.ROUND);
         brush.setStrokeWidth(20f);
 
         // brush to draw user character
@@ -62,10 +78,25 @@ public class PaintView extends View {
         brushIcon.setColor(getResources().getColor(R.color.colorUserIcon));
         brushIcon.setStyle(Paint.Style.STROKE);
         brushIcon.setStrokeJoin(Paint.Join.ROUND);
+        brushIcon.setStrokeCap(Paint.Cap.ROUND);
         brushIcon.setStrokeWidth(50f);
+
+        //
+        brushArrow.setAntiAlias(true);
+        brushArrow.setColor(getResources().getColor(R.color.colorUserIcon));
+        brushArrow.setStyle(Paint.Style.STROKE);
+        brushArrow.setStrokeJoin(Paint.Join.ROUND);
+        brushArrow.setStrokeCap(Paint.Cap.ROUND);
+        brushArrow.setStrokeWidth(15f);
 
         // layout settings
         params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+        // get direction image from drawable
+        directionImage = ResourcesCompat.getDrawable(getResources(), R.drawable.small_arrow, null);
+        directionImage.setColorFilter(new PorterDuffColorFilter(
+                getResources().getColor(R.color.colorDirectionArrow),PorterDuff.Mode.MULTIPLY));
+        imageRange = 7*iconRadius;
 
         // run method every second
         updateTimer = new Timer();
@@ -81,7 +112,7 @@ public class PaintView extends View {
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas)  {
         // canvas background color
         canvas.drawColor(getResources().getColor(R.color.colorCanvasBackground));
 
@@ -97,7 +128,11 @@ public class PaintView extends View {
         }
 
         canvas.drawPath(path, brush);
-        canvas.drawCircle(pointX, pointY, 10, brushIcon);
+        canvas.drawCircle(pointX, pointY, iconRadius, brushIcon);
+
+        directionImage.setBounds( (int)(pointX+35), (int)(pointY-35), (int)(pointX+105), (int)(pointY+35));
+        directionImage.draw(canvas);
+
     }
 
     // update points, paths and refresh canvas
@@ -120,9 +155,11 @@ public class PaintView extends View {
 
 
 
+
+// TRIED METHODS
+
 // FOR SOME INFO
 // TOUCH FOR TESTING IF IT WORKS
-// DELETE LATER
     /*
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -146,4 +183,54 @@ public class PaintView extends View {
         postInvalidate();
         return false;
     }
+     */
+
+
+// DRAWING ARROW IN THE DIRECTION (NOT WORKING GOOD ENOUGH)
+
+    /*
+    private void assignArrowPoints(int hDir, int vDir)
+    {
+        // first point assigned
+        arrowFirstPointX = pointX + (horizontalDir * (10 * iconRadius));
+        arrowFirstPointY = pointY - (verticalDir * (10 * iconRadius));
+
+        // directions from the user character circle
+        int pointSecond_hDir = 0, pointSecond_vDir = 0;
+        int pointThird_hDir = 0, pointThird_vDir = 0;
+
+        if(hDir != 0 && vDir != 0)
+        {
+            // this covers 4 directions
+            pointSecond_hDir = -hDir;
+            pointSecond_vDir = vDir;
+            pointThird_hDir = hDir;
+            pointThird_vDir = -vDir;
+        }
+        else if(hDir == 0)
+        {
+            // this covers 2 directions
+            pointSecond_hDir = 1;
+            pointSecond_vDir = 0;
+            pointThird_hDir = -1;
+            pointThird_vDir = 0;
+        }
+        else if(vDir == 0)
+        {
+            // this covers 2 directions
+            pointSecond_hDir = 0;
+            pointSecond_vDir = 1;
+            pointThird_hDir = 0;
+            pointThird_vDir = -1;
+        }
+
+        // assign second arrow point
+        arrowSecondPointX = pointX + (pointSecond_hDir * (6 * iconRadius));
+        arrowSecondPointY = pointY - (pointSecond_vDir * (6 * iconRadius));
+
+        // assign third arrow point
+        arrowThirdPointX = pointX + (pointThird_hDir * (6 * iconRadius));
+        arrowThirdPointY = pointY - (pointThird_vDir * (6 * iconRadius));
+    }
+
      */
